cd29c4a3490a7954cd1015e26d55ee8647dd671d
ios-1 <ios-1@hebodemacbook-air.local>
2018-01-07 16:50:06 +0800

    Initial Commit

---
+++ FangXinCai/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FangXinCai
+//
+//  Created by ios-1 on 2018/1/7.
+//  Copyright © 2018年 ios-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FangXinCai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FangXinCai
+//
+//  Created by ios-1 on 2018/1/7.
+//  Copyright © 2018年 ios-1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
